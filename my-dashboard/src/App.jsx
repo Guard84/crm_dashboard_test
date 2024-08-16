af291a147ad055d6c7228c56aa9baa88bff48555
Guard84 <guardsofword@gmail.com>
2024-08-16 16:43:27 +0300

    fixed table

--- my-dashboard/src/App.jsx
+++ my-dashboard/src/App.jsx
@@ -2,13 +2,17 @@
 import './styles/app.scss'
 import Sidebar from './components/Sidebar'
 import Customers from './components/Customers'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className="app-container">
       <Sidebar />
+      <div>
+      <Header/>
       <Customers />
+      </div>
     </div>
   )
 }
