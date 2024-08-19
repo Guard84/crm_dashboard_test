f9797d7f6de3532811c8bd039b05f1e4ee8e449a
Guard84 <guardsofword@gmail.com>
2024-08-19 15:22:14 +0300

    fixed avatar

--- my-dashboard/src/components/Sidebar.jsx
+++ my-dashboard/src/components/Sidebar.jsx
@@ -29,7 +29,7 @@ const Sidebar = () => {
         <Menu />
         <div className="sidebar-manager">
           <img
-            src="/images/manager_photo.png"
+            src="/crm_dashboard_test/images/manager_photo.png"
             alt="Manager avatar"
           />
           <div className="sidebar-manager-info">
