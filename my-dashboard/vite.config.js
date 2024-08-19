4f46026e34e63fd52946fa4f9e50ca9746de9e4d
Guard84 <guardsofword@gmail.com>
2024-08-19 15:16:32 +0300

    Prepare for GitHub Pages deployment

--- my-dashboard/vite.config.js
+++ my-dashboard/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/crm_dashboard_test/',
   build: {
-  sourcemap: true,
- }
+    sourcemap: true,
+    outDir: 'dist',
+  }
 })
